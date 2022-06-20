cace385d15fc39e63f8a8534e87fa52a2906f153
Sean Mollet <sean@malmoset.com>
2022-06-20 20:14:48 +0000

    Add a script to make a squashfs

--- launchdocker.sh
+++ launchdocker.sh
@@ -9,6 +9,10 @@ mkdir -p deploy
 docker stop pi-gen
 docker rm pi-gen
 docker run -d \
+--privileged \
+--cap-add=ALL \
+-v /lib/modules:/lib/modules \
+-v /dev:/dev \
 -v $PWD:/pi-gen \
 --name pi-gen seanmollet/pi-gen
 
