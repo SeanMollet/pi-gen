2a5bd68144a263f02f357133bcdcf71fa99ba31b
Sean Mollet <sean@malmoset.com>
2022-06-22 18:39:26 +0000

    Let's actually do this..

--- minimal.sh
+++ minimal.sh
@@ -2,9 +2,9 @@
 touch stage3/SKIP
 touch stage3/SKIP_IMAGES
 touch stage4/SKIP
-touch stage4/SKIP_IMAGE
+touch stage4/SKIP_IMAGES
 touch stage5/SKIP
-touch stage5/SKIP_IMAGE
+touch stage5/SKIP_IMAGES
 
 rm stage2/EXPORT_NOOBS
 rm stage4/EXPORT_NOOBS
