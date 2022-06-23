27367d6c48b7cc5e86f7505007430a4e16bb8b69
Sean Mollet <sean@malmoset.com>
2022-06-23 15:55:12 +0000

    Remove apt-cacher-ng, it's broken

--- launchdocker.sh
+++ launchdocker.sh
@@ -16,15 +16,4 @@ docker run -d \
 -v $PWD:/pi-gen \
 --name pi-gen seanmollet/pi-gen
 
-CONTAINER_NAME='apt-cacher-ng'
-CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$)
-if [ ! "${CID}" ]; then
-	echo Launching apt-cacher-ng
-	docker build -t sameersbn/apt-cacher-ng github.com/sameersbn/docker-apt-cacher-ng
-	docker run --name apt-cacher-ng --init -d --restart=always \
-	  --publish 3142:3142 \
-	  --volume apt-cacher-ng-data:/var/cache/apt-cacher-ng \
-	  sameersbn/apt-cacher-ng || docker start apt-cacher-ng
-fi
-	  
   
