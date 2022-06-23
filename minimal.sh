092a022b7ad9af32c9fc8fb0b4e19614c76a8103
Sean Mollet <sean@malmoset.com>
2022-06-23 16:14:06 +0000

    No need to delete it.. just don't do it (skip stage 2 image export)

--- minimal.sh
+++ minimal.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+touch stage2/SKIP_IMAGES
 touch stage3/SKIP
 touch stage3/SKIP_IMAGES
 touch stage4/SKIP
@@ -9,5 +10,3 @@ touch stage5/SKIP_IMAGES
 rm stage2/EXPORT_NOOBS
 rm stage4/EXPORT_NOOBS
 rm stage5/EXPORT_NOOBS
-
-rm stage2/EXPORT_IMAGE
